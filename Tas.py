64afa90adeda5fbbdede60c186853fc2a246a62e
amirfallhicode <www.amirhoseen.falahiii97@gmail.com>
2025-12-15 15:26:19 +0330

    Create Tas.py

---
+++ Tas.py
@@ -0,0 +1,11 @@
+import random
+c=1
+while True:
+    e=random.randit(1,6)
+    f=int(input("adad bede")
+    if e==f:
+        print("تعداد دفعات",c)
+        break
+    else:        
+         print("you failed")
+         c+=1
